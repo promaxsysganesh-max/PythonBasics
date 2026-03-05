8d4b8ed3bf96b541c28e66ea399b14e844815266
promaxsysganesh-max <promaxsys.ganesh@gmail.com>
2026-03-05 16:17:23 +0530

    Inheritance and polymorphism

---
+++ OOPInheritancePolymorphism.py
@@ -0,0 +1,24 @@
+# class Person:
+#     def greet(self):
+#         print("Hello")
+
+# class Student(Person):
+#     pass
+
+# s1 = Student()
+# s1.greet()
+
+
+class Dog:
+    def sound(self):
+        print("Bark")
+
+class Cat:
+    def sound(self):
+        print("Meow")
+
+d = Dog()
+c = Cat()
+
+d.sound()
+c.sound()
